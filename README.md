b815bcc28d65518e440fbf6df95c99a6203c5a3c
hamatai7109 <taishihamano0515@gmail.com>
2023-10-12 17:19:46 +0900

    added the function of edit, delete, filter

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 I tried to use Next.js without enough experience with React.
 So now I am on the basic app "TODO app", which is simple but pretty helpful for beginners to grab comprehensive understandings of React.
+
+See what I will get.
--- src/App.js
+++ src/App.js
@@ -1,52 +1,98 @@
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
 import Todo from "./components/Todo";
+import { nanoid, nanoids } from "nanoid";
+import React, { useState } from "react";
+
+//App関数の外でフィルターを定義することで、レンダリングの影響を受けなくする。
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+  // tasksの内容を保持する。初期値はindex.jsから受け取ったDATAの値。
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  // チェックボタンの切り替えを保持
+  function toggleTaskCompleted(id) {
+    const updateTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updateTasks);
+  }
+
+  // タスク編集機能
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  // タスク削除機能
+  // Todo.js内で呼び出されたタスクのid「以外」の要素だけで、新しい配列「remainingTasks」を作成している。
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  // index.jsで定義した「DATA」をpropsとして受け取り、map関数を用いて一覧表示している。その際に、Todoコンポーネントへ各項目をpropsとして渡している。
+  // DATA(index.js) → taskList(App.js) → id,name,completed(Todo.js) : 親コンポーネントから子へと一方向の流れ
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id} //You should always pass a unique key to anything you render with iteration
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  // タスクのフィルター機能
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  //taskの数によって、単数形と複数形の表示を変える。
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  // タスクの追加機能
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="eat" completed={true} id="todo-0" />s
-        <Todo name="sleep" completed={false} id="todo-1" />
-        <Todo name="run" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   );
